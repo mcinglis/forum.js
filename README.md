a864de945c3fd9f5f55a4d3a77c3115fdb46f50c
Malcolm Inglis <me@minglis.id.au>
2013-05-12 18:49:04 +1000

    Add more basic functionality.

---
+++ README.md
@@ -0,0 +1,5 @@
+Forum.js is a basic forum server based on [Express](http://expressjs.com) and [Mongoose](http://mongoosejs.com). There's probably already a project called forum.js, but whatevs.
+
+This was inspired by [fwielstra's gist](https://gist.github.com/fwielstra/1025038) of the same theme. I've just forked that, cleaned it up (IMHO), and added some functionality.
+
+So, currently, this is just an improved demo of Express and Mongoose. In future, I might flesh it out a bit more, which is why I made a repository for this.
--- forum.js
+++ forum.js
@@ -7,44 +7,87 @@ var mongoose = require( 'mongoose' );
 
 var app = express();
 
-mongoose.connect( 'mongodb://localhost/forum' );
-
 app.set( 'port', 3000 );
+app.set( 'mongodb', 'mongodb://localhost/test' );
 
+app.use( express.logger( 'dev' ) );
 app.use( express.bodyParser() );
 app.use( express.methodOverride() );
 
 var Thread = mongoose.model( 'Thread', {
     title: String,
     author: String,
-    created: { type: Date, default: Date.now },
-    posts: [{
-        text: String,
-        author: String,
-        created: { type: Date, default: Date.now }
-    }]
+    created: { type: Date, default: Date.now }
 });
 
+var Post = mongoose.model( 'Post', {
+    threadId: {
+        type: mongoose.Schema.ObjectId,
+        // There must be a thread with the given id.
+        validate: function( value, respond ) {
+            Thread.findById( value, function( error, thread ) {
+                // Respond with false if a thread doesn't exist with that id.
+                respond( error ? false : ( thread !== null ) );
+            });
+        }
+    },
+    text: String,
+    author: String,
+    created: { type: Date, default: Date.now }
+});
+
+// These route handlers don't account for HTTP response codes, security,
+// authorization or performance. They're just demonstrative.
+
+var sendErrorOrDocument = function( response ) {
+    return function( error, document ) {
+        response.send( error || document );
+    };
+};
+
 app.get( '/threads', function( request, response ) {
-    Thread.find(function( error, threads ) {
-        response.send( error || threads );
-    });
+    Thread.find( sendErrorOrDocument( response ) );
 });
 
 app.post( '/threads', function( request, response ) {
-    Thread.create({
-        title: request.body.title,
-        author: request.body.author
-    }, function( error, thread ) {
-        response.send( error || thread );
-    });
+    Thread.create( request.body, sendErrorOrDocument( response ) );
 });
 
 app.get( '/threads/:id', function( request, response ) {
-    Thread.findById( request.params.id, function( error, thread ) {
-        response.send( error || thread );
+    Thread.findById( request.params.id, function( threadError, thread ) {
+        if ( threadError ) {
+            return response.send( threadError );
+        }
+        Post.find({ threadId: thread._id }, function( postError, posts ) {
+            response.send( postError || { thread: thread, posts: posts } );
+        });
     });
 });
+
+app.patch( '/threads/:id', function( request, response ) {
+    Thread.findByIdAndUpdate(
+        request.params.id,
+        { $set: request.body },
+        sendErrorOrDocument( response )
+    );
+});
+
+app.post( '/posts', function( request, response ) {
+    Post.create(
+        request.body,
+        sendErrorOrDocument( response )
+    );
+});
+
+app.patch( '/posts/:id', function( request, response ) {
+    Post.findByIdAndUpdate(
+        request.params.id,
+        { $set: request.body },
+        sendErrorOrDocument( response )
+    );
+});
+
+mongoose.connect( app.get( 'mongodb' ) );
 
 app.listen( app.get( 'port' ), function() {
     console.log( 'Forum server listening on port ' + app.get( 'port' ) );
